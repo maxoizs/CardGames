f0fc3e0bde7394246c9123e977f35a2330602c25
Mohammad Mahmoud <abdalaal.mohamed@gmail.com>
2018-02-03 00:37:12 +0000

    refactor

--- src/CardGame/CardGame.cs
+++ src/CardGame/CardGame.cs
@@ -1,11 +1,56 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CardGame
 {
-    public abstract class CardGame
+    public class CardGame : ICardGame
     {
-        public List<ICardPlayer> Players { get; protected set; }
         public List<Card> Cards { get; protected set; }
         public List<Card> Pile { get; protected set; }
+
+        public CardGame()
+        {
+            Pile = new List<Card>();
+            Cards = new List<Card>();
+        }
+
+        public virtual void Start(params ICardPlayer[] players)
+        {
+            PopulateCards();
+            DealCards(players);
+        }
+
+        public virtual void Play(ICardPlayer player)
+        {
+            var card = player.Play();
+            Pile.Add(card);
+        }
+
+        protected void PopulateCards()
+        {
+            var cards = new List<Card>();
+            foreach (var cardName in Enum.GetValues(typeof(CardName)).Cast<CardName>())
+            {
+                cards.Add(new Card { Color = CardColor.Black, Name = cardName, Shape = CardShape.Spades });
+                cards.Add(new Card { Color = CardColor.Red, Name = cardName, Shape = CardShape.Hearts });
+                cards.Add(new Card { Color = CardColor.Black, Name = cardName, Shape = CardShape.Diamonds });
+                cards.Add(new Card { Color = CardColor.Red, Name = cardName, Shape = CardShape.Clubs });
+            }
+            var rand = new Random();
+            Cards = cards.ProduceShuffle(rand);
+        }
+
+        protected void DealCards(params ICardPlayer[] players)
+        {
+            int count = Cards.Count / players.Length;
+
+            // either deal all at once or one by one
+            foreach (var player in players)
+            {
+                player.Deal(Cards.Take(count));
+                Cards.RemoveAll(c => player.Cards.Contains(c));
+            }
+        }
     }
 }
--- src/CardGame/Snap.cs
+++ src/CardGame/Snap.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,57 +8,10 @@ namespace CardGame
     /// </summary>
     public class SnapGame : CardGame, ISnapGame
     {
-        public SnapGame()
-        {
-            Players = new List<ICardPlayer>();
-            Pile = new List<Card>();
-            Cards = new List<Card>();
-        }
-
-        public void Start(params ICardPlayer[] players)
-        {
-           
-            PopulateCards();
-            SplitCards(players);
-            Players = players.ToList();
-        }
-
-        public void Play(ICardPlayer player)
-        {
-            var card = player.Play();
-            Pile.Add(card);
-        }
-
         public void Snap(ISnapPlayer player)
         {
             player.Snap(Pile);
             Pile.Clear();
-        }
-
-        private void SplitCards(params ICardPlayer[] players)
-        {
-            int count = Cards.Count / players.Length;
-
-            // either deal all at once or one by one
-            foreach (var player in players)
-            {
-                player.Deal(Cards.Take(count));
-                Cards.RemoveAll(c=> player.Cards.Contains(c));
-            }
-        }
-
-        private void PopulateCards()
-        {
-            var cards = new List<Card>();
-            foreach (var cardName in Enum.GetValues(typeof(CardName)).Cast<CardName>())
-            {
-                cards.Add(new Card {Color = CardColor.Black, Name = cardName, Shape = CardShape.Spades});
-                cards.Add(new Card {Color = CardColor.Red, Name = cardName, Shape = CardShape.Hearts});
-                cards.Add(new Card {Color = CardColor.Black, Name = cardName, Shape = CardShape.Diamonds });
-                cards.Add(new Card {Color = CardColor.Red, Name = cardName, Shape = CardShape.Clubs});
-            }
-            var rand = new Random();
-            Cards = cards.ProduceShuffle(rand);
         }
     }
 }
